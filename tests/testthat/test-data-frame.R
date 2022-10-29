72b5a2b538e8e88544af5d57925b0587abaf2b1d
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-10-29 19:58:13 -0400

    add df tests

---
+++ tests/testthat/test-data-frame.R
@@ -0,0 +1,21 @@
+test_that("quick_df()", {
+  expect_error(quick_df(1L))
+  expect_error(quick_df(list(a = 1:2, b = 1:3)))
+
+  expect_identical(
+    quick_df(list(a = NULL)),
+    struct(list(a = NULL), "data.frame", names = "a", row.names = integer())
+  )
+
+  expect_identical(
+    quick_df(list(a = integer())),
+    data.frame(a = integer(), stringsAsFactors = FALSE)
+  )
+
+  expect_equal(
+    quick_df(list(a = integer())),
+    quick_dfl(a = integer())
+  )
+
+  expect_identical(quick_df(NULL), empty_df())
+})
