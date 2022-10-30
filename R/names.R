d9bd012751bfedab1d428551cad698bfc6467b2a
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-10-29 21:07:49 -0400

    trim down names

--- R/names.R
+++ R/names.R
@@ -3,25 +3,6 @@ is_named <- function(x) {
   !is.null(names(x))
 }
 
-#' Sort by names
-#'
-#' Sort a vector by it's name
-#'
-#' @param x A named vector of values
-#' @param numeric If `TRUE` will try to coerce to numeric
-#' @return `x` sorted by its `names()`
-#' @export
-sort_names <- function(x, numeric = FALSE) {
-  check_is_vector(x)
-  nm <- names(x) %||% stop("x must be a named vector", call. = FALSE)
-
-  if (numeric) {
-    nm <- as.numeric(nm)
-  }
-
-  sort_by(x, nm)
-}
-
 #' Set names
 #'
 #' Sets or removes names
@@ -29,31 +10,24 @@ sort_names <- function(x, numeric = FALSE) {
 #' @param x A vector of values
 #' @param nm A vector of names
 #' @return
-#' * `set_names0()`: `x` with `nm` values assigned to names (if `x` is `NULL`, `NULL` is returned)
+#' * `set_names()`: `x` with `nm` values assigned to names (if `x` is `NULL`, `NULL` is returned)
 #' * `remove_names()`: `x` without names
 #' * `names_switch()`: `character` vector of equal length `x` where names and values are switched
 #'
 #' @export
-set_names0 <- function(x, nm = x) {
+set_names <- function(x, nm = x) {
   if (is.null(x)) return(NULL)
   `names<-`(x, nm)
 }
 
-#' @rdname set_names0
+#' @rdname set_names
 #' @export
 remove_names <- function(x) {
-  set_names0(x, NULL)
+  `names<-`(x, NULL)
 }
 
-#' @rdname set_names0
-#' @export
-names_switch <- function(x) {
-  nm <- names(x) %||% stop("x must be named", call. = FALSE)
-  set_names0(nm, as.vector(x, "character"))
-}
-
-#' @rdname set_names0
+#' @rdname set_names
 #' @export
 `%names%` <- function(x, nm) {
-  set_names0(x, nm)
+  set_names(x, nm)
 }
