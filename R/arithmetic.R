bedaa4b877fc978b6ba3e5d646ef030e613de020
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-10-29 21:58:30 -0400

    add arithmetic and extract aliases

---
+++ R/arithmetic.R
@@ -0,0 +1,37 @@
+#' Arithmetic wrappers
+#'
+#' @inheritParams base::Arithmetic
+#' @name alias_arithmetic
+NULL
+
+# nocov start
+
+#' @export
+#' @rdname alias_arithmetic
+add <- `+`
+
+#' @export
+#' @rdname alias_arithmetic
+subtract <- `-`
+
+#' @export
+#' @rdname alias_arithmetic
+multiply <- `*`
+
+#' @export
+#' @rdname alias_arithmetic
+divide <- `/`
+
+#' @export
+#' @rdname alias_arithmetic
+raise_power <- `^`
+
+#' @export
+#' @rdname alias_arithmetic
+remainder <- `%%`
+
+#' @export
+#' @rdname alias_arithmetic
+divide_int <- `%/%`
+
+# nocov end
---
+++ R/extract.R
@@ -0,0 +1,33 @@
+#' Extract and replace aliases
+#'
+#' @inheritParams base::Extract
+#' @name alias_extract
+NULL
+
+# nocov start
+
+#' @export
+#' @rdname alias_extract
+subset1 <- `[`
+
+#' @export
+#' @rdname alias_extract
+subset2 <- `[[`
+
+#' @export
+#' @rdname alias_extract
+subset2 <- `$`
+
+#' @export
+#' @rdname alias_extract
+subassign1 <- `[<-`
+
+#' @export
+#' @rdname alias_extract
+subassign2 <- `[[<-`
+
+#' @export
+#' @rdname alias_extract
+subassign3 <- `$<-`
+
+# nocov end
