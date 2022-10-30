1e2a7bf1cbe02f07b3a3cf369cc16124a9797c67
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-10-29 21:29:45 -0400

    add descriptions and nocov for os

--- R/os.R
+++ R/os.R
@@ -1,11 +1,29 @@
+
+#' Determine operating systems
+#'
+#' @returns `TRUE` or `FALSE`
+#' @export
+#' @name os
+NULL
+
+# ncov start
+
+#' @export
+#' @rdname os
 is_windows <- function() {
   Sys.info()[["sysname"]] == "Windows"
 }
 
+#' @export
+#' @rdname os
 is_macos <- function() {
   Sys.info()[["sysname"]] == "Darwin"
 }
 
+#' @export
+#' @rdname os
 is_linux <- function() {
   Sys.info()[["sysname"]] == "Linux"
 }
+
+# nocov end
