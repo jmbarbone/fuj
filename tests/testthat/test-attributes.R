7c7d72dbd8b915a96bb8400de069517c741fdbac
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-10-29 21:59:46 -0400

    correct expected value

--- tests/testthat/test-attributes.R
+++ tests/testthat/test-attributes.R
@@ -1,6 +1,6 @@
 test_that("exattr", {
   x <- struct(list(), "foo", aa = 1, a = 2)
-  expect_identical(x %attr% "a", 1)
+  expect_identical(x %attr% "a", 2)
 
   x <- struct(list(), "foo", aa = 1)
   expect_null(x %attr% "a")
