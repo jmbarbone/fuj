778c3bbbb07e710f38542a6b112b8acda00eb64c
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-11-25 12:32:07 -0500

    [#12] updates condition tests

--- tests/testthat/test-colons.R
+++ tests/testthat/test-colons.R
@@ -1,6 +1,6 @@
 test_that("%colons%", {
-  expect_error("fuj" %colons% "not_in_fuj", class = "colonsError")
-  expect_error("1" %colons% "foo", class = "namespaceError")
+  expect_error("fuj" %colons% "not_in_fuj", class = "fuj:colonsError")
+  expect_error("1" %colons% "foo", class = "fuj:namespaceError")
 
   expect_identical("base" %colons% "[", match.fun("["))
   expect_identical("base" %colons% "letters", letters)
--- tests/testthat/test-namespace.R
+++ tests/testthat/test-namespace.R
@@ -1,4 +1,4 @@
 test_that("require_namespace", {
-  expect_error(require_namespace("1"), class = "namespaceError")
+  expect_error(require_namespace("1"), class = "fuj:namespaceError")
   expect_error(require_namespace("base"), NA)
 })
