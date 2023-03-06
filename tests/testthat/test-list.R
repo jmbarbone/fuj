4f03b4fe9f075014e06ceedcd1fc7ed3c35fc24c
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-03-05 22:26:01 -0500

    #19 add tests

--- tests/testthat/test-list.R
+++ tests/testthat/test-list.R
@@ -4,4 +4,7 @@ test_that("list0", {
 
   foo <- function(...) list0(...)
   expect_identical(foo(1, 2, "c", ), list(1, 2, "c"))
+
+  expect_error(list0(1, `__not_a_variable__`))
+  expect_error(list0(1, `__not_a_variable__`, ))
 })
