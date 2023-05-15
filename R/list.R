955a9d9350d3457940b06f72e2b3b3f999f471e1
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-05-14 23:35:31 -0400

    #25 include names in list0()

--- R/list.R
+++ R/list.R
@@ -7,10 +7,12 @@
 #' @examples
 #' try(list(1, ))
 #' list0(1, )
+#' list0(a = 1, )
 #' @export
 list0 <- function(...) {
   mc <- match.call()
   out <- list()
+  off <- 0L
 
   for (i in seq_len(...length())) {
     tryCatch(
@@ -20,9 +22,12 @@ list0 <- function(...) {
           e$call <- mc
           stop(e)
         }
+
+        assign("off", 1L, parent.frame()$parentenv)
       }
     )
   }
 
+  names(out) <- names(mc)[-1L][seq_len(i - off)]
   out
 }
