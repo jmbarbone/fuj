e98ac8ae01b86a699628d30442663a5f0d2fe456
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-07-04 23:24:14 -0400

    #29 Create verbose.R

---
+++ R/verbose.R
@@ -0,0 +1,24 @@
+#' Verbose
+#'
+#' Show messages when verbose
+#'
+#' @param ...,.fun Configurations for how messages will displayed
+#' @returns The result of `.fun` when `getOption("verbose")` is `TRUE`,
+#'   otherwise nothing, invisibly
+#' @export
+#' @examples
+#' op <- options(verbose = FALSE)
+#' verbose("no message")
+#' options(verbose = TRUE)
+#' verbose("message")
+#' options(op)
+
+verbose <- function(..., .fun = "message") {
+  if (isTRUE(getOption("verbose"))) {
+    if (!(is.null(..1) && ...length() == 1L)) {
+      return(match.fun(.fn)(...))
+    }
+  }
+
+  invisible()
+}
