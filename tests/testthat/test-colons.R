a63ad651355a7b4b57d14fc8fc954874cbe044f4
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-07-21 11:29:31 -0400

    #31 Update test-colons.R

--- tests/testthat/test-colons.R
+++ tests/testthat/test-colons.R
@@ -1,7 +1,13 @@
 test_that("%colons%", {
-  expect_error("fuj" %colons% "not_in_fuj", class = "fuj:colonsError")
-  expect_error("1" %colons% "foo", class = "fuj:namespaceError")
+  expect_identical(`%colons%`, `%:::%`)
 
-  expect_identical("base" %colons% "[", match.fun("["))
-  expect_identical("base" %colons% "letters", letters)
+  # errors
+  expect_error("fuj" %:::% "not_in_fuj", class = "fuj:colonsError")
+  expect_error("fuj" %::% "colons_example", class = "fuj:colonsError")
+  expect_error("1" %::% "foo", class = "fuj:namespaceError")
+
+  # successes
+  expect_identical("fuj" %:::% "colons_example", "Hello, world")
+  expect_identical("base" %::% "[", match.fun("["))
+  expect_identical("base" %::% "letters", letters)
 })
