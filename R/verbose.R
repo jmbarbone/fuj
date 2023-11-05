4415f157d3750c199802090fedff9903727733db
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-11-05 13:04:28 -0500

    add enhancements (#36)

--- R/verbose.R
+++ R/verbose.R
@@ -7,8 +7,16 @@
 #'   (the default) and an override, `options(fuj.verbose)`.  The latter can be
 #'   set to a function whose result will be used for conditional evaluation.
 #'
+#'   [make_verbose()] allows for the creation of a custom verbose function.
+#'
 #' @param ... A message to display.  When `...` is `NULL` (and only `NULL`), no
 #'   message will display.
+#' @param .fill When `TRUE`, each new line will be prefixed with the verbose
+#'   label (controlled through `options(fuj.verbose.fill)`)
+#' @param .label A label to prefix the message with (controlled through
+#'   `options(fuj.verbose.label)`).  Note: label always ends with a single
+#'   space.  Trailing spaces are stripped, and a single space is added, if
+#'   needed.
 #' @returns None, called for its side-effects.  When conditions are met, will
 #'   signal a `verboseMessage` condition.
 #' @examples
@@ -28,8 +36,18 @@
 #' "`if` returns `NULL` when not `TRUE`, which makes for additional control"
 #' })
 #' options(op)
+#'
+#' # make your own verbose
+#' verb <- make_verbose("fuj.foo.bar")
+#' verb("will not show")
+#' options(fuj.foo.bar = TRUE)
+#' verb("will show")
 #' @export
-verbose <- function(...) {
+verbose <- function(
+    ...,
+    .fill = getOption("fuj.verbose.fill", FALSE),
+    .label = getOption("fuj.verbose.label", "[verbose]")
+) {
   op <- getOption("fuj.verbose", getOption("verbose"))
 
   if (is.function(op)) {
@@ -38,16 +56,64 @@ verbose <- function(...) {
 
   if (isTRUE(op)) {
     if (!(is.null(..1) && ...length() == 1L)) {
-      message(verbose_message(...))
+      message(verbose_message(..., .fill = .fill, .label = .label))
     }
   }
 
   invisible()
 }
 
-verbose_message <- function(..., call = NULL) {
+#' @param opt An option to use in lieu of `fun.verbose`.  Note:
+#'   `options(fuj.verbose)` is temporarily set to `isTRUE(getOption(opt))` when
+#'   the function is evaluate, but is reset to its original value on exit.
+#' @rdname verbose
+#' @export
+make_verbose <- function(opt) {
+  force(opt)
+  as.function(
+    c(alist(
+      ... = ,
+      .fill = getOption("fuj.verbose.fill", FALSE),
+      .label = getOption("fuj.verbose.label", "[verbose]")
+    ), substitute({
+      op <- options(fuj.verbose = isTRUE(getOption(opt)))
+      on.exit(options(op))
+      verbose(
+        ...,
+        .fill = getOption("fuj.verbose.fill", FALSE),
+        .label = getOption("fuj.verbose.label", "[verbose]")
+      )
+    }))
+  )
+}
+
+verbose_message <- function(
+    ...,
+    .fill = getOption("fuj.verbose.fill", FALSE),
+    .label = getOption("fuj.verbose.label", "[verbose]"),
+    .call = NULL
+) {
+  if (is.function(.label)) {
+    .label <- eval(.label(), envir = parent.frame())
+    .label <- as.character(.label)
+    stopifnot(length(.label) == 1L)
+  }
+
+  # make sure to add a space
+  .label <- sub("[[:space:]]{0,}$", " ", .label)
+
+  msg <-
+    if (isTRUE(.fill)) {
+        con <- file()
+        on.exit(close(con))
+        cat(..., file = con)
+        paste0(.label, readLines(con, warn = FALSE), collapse = "\n")
+    } else {
+      .makeMessage(.label, ...)
+    }
+
   struct(
-    list(.makeMessage("[verbose] ", ..., appendLF = TRUE), call),
+    list(msg, call),
     names = c("message", "call"),
     class = c("verboseMessage", "message", "condition")
   )
