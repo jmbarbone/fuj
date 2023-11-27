710b363ba80eecb0926c34bcc53c93514ec114f4
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-11-26 22:14:09 -0500

    Create fuj-package.R (#42)

---
+++ R/fuj-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+op.fuj <- list(
+  fuj.verbose = NULL,
+  fuj.verbose.fill = FALSE,
+  fuj.verbose.label = "[verbose] "
+)
