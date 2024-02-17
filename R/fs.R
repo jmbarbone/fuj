892f13992e96737f87d8229475a5d66c10a47806
Jordan Mark Barbone <jmbarbone@gmail.com>
2024-02-17 12:03:37 -0500

    improve docs/aliases/examples (#53)

--- R/fs.R
+++ R/fs.R
@@ -2,18 +2,25 @@
 #'
 #' Lightweight file path functions
 #'
-#' @description [is_path()] checks for either a `path` class or an `fs_path`,
-#' the latter useful for the `fs` package.
+#' @description [is_path()] checks for either a `file_path` class or an
+#'   `fs_path`, the latter useful for the `fs` package.
+#'
+#'   [file_path()] is an alias for [fp()] and [is_file_path()] is an alias for
+#'   [is_path()].
 #'
 #' @param ... Path components, passed to [file.path()]
 #' @param x An object to test
 #' @return
-#' - [fp()]: A `character` vector of the normalized path with a "path" class
-#' - [is_path()]: A `TRUE` or `FALSE` value
+#' - [fp()]/[file_path()]: A `character` vector of the normalized path with a
+#' `"file_path"` class
+#' - [is_path()]/[is_file_path()]: A `TRUE` or `FALSE` value
 #' @export
 #' @examples
 #' fp("here")
 #' fp("~/there")
+#' fp("back\\slash")
+#' fp("remove//extra\\\\slashes")
+#' fp("a", c("b", "c"), "d")
 fp <- function(...) {
   x <- normalizePath(file.path(..., fsep = "/"), "/", FALSE)
   x <- gsub("\\", "/", x, fixed = TRUE)
@@ -22,13 +29,21 @@ fp <- function(...) {
 }
 
 #' @export
-print.path <- function(x, ...) {
-  cat(x, "\n")
+#' @rdname fp
+file_path <- fp
+
+#' @export
+print.file_path <- function(x, ...) {
+  writeLines(x)
   invisible(x)
 }
 
 #' @export
 #' @rdname fp
 is_path <- function(x) {
-  inherits(x, c("path", "fs_path"))
+  inherits(x, c("file_path", "fs_path"))
 }
+
+#' @export
+#' @rdname fp
+is_file_path <- is_path
