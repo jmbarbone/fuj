e00ad95fdf42709f8b0ea943c839cfd4a40d0e82
Jordan Mark Barbone <jmbarbone@gmail.com>
2024-02-21 00:23:41 -0500

    separate tests if on windows (#53)

--- tests/testthat/test-fs.R
+++ tests/testthat/test-fs.R
@@ -1,22 +1,50 @@
 test_that("fs works", {
   expect_true(is_path(fp("here")))
   expect_true(is_file_path(file_path("here")))
+})
+
+test_that("fs works on windows", {
+  skip_if_not(is_windows())
+
+  np <- function(x) normalizePath(x, winslash = "/", FALSE)
 
   expect_identical(
     fp("~/here///there"),
-    path.expand("~/here/there"),
+    np("~/here/there"),
     ignore_attr = TRUE
   )
 
   expect_identical(
     fp("~/here//there\\\\where"),
-    path.expand("~/here/there/where"),
+    np("~/here/there/where"),
     ignore_attr = TRUE
   )
 
   expect_identical(
     fp("~/a", c("b", "c"), "d"),
-    path.expand(c("~/a/b/d", "~/a/c/d")),
+    np(c("~/a/b/d", "~/a/c/d")),
+    ignore_attr = TRUE
+  )
+})
+
+test_that("fs works on not windows", {
+  skip_if(is_windows())
+
+  expect_identical(
+    fp("here///there"),
+    "here/there",
+    ignore_attr = TRUE
+  )
+
+  expect_identical(
+    fp("here//there\\\\where"),
+    "here/there/where",
+    ignore_attr = TRUE
+  )
+
+  expect_identical(
+    fp("a", c("b", "c"), "d"),
+    c("a/b/d", "a/c/d"),
     ignore_attr = TRUE
   )
 })
