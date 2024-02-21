4eb841cb6a4bcf718118cd8e8995b9a3362691bb
Jordan Mark Barbone <jmbarbone@gmail.com>
2024-02-21 00:15:00 -0500

    use path expansions (#53)

--- tests/testthat/test-fs.R
+++ tests/testthat/test-fs.R
@@ -3,24 +3,25 @@ test_that("fs works", {
   expect_true(is_file_path(file_path("here")))
 
   expect_identical(
-    fp("here///there"),
-    "here/there",
+    fp("~/here///there"),
+    path.expand("~/here/there"),
     ignore_attr = TRUE
   )
 
   expect_identical(
-    fp("here//there\\\\where"),
-    "here/there/where",
+    fp("~/here//there\\\\where"),
+    path.expand("~/here/there/where"),
     ignore_attr = TRUE
   )
 
   expect_identical(
-    fp("a", c("b", "c"), "d"),
-    c("a/b/d", "a/c/d"),
+    fp("~/a", c("b", "c"), "d"),
+    path.expand(c("~/a/b/d", "~/a/c/d")),
     ignore_attr = TRUE
   )
 })
 
 test_that("snapshots", {
+  skip_if(is_windows())
   expect_snapshot(fp(c("here/there", "every/where", "good\\slash")))
 })
