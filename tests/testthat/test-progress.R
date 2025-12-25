4e53f6e57739f5d06a71efe0955c7d3eb36d4687
Jordan Mark Barbone <jmbarbone@gmail.com>
2025-12-24 23:48:29 -0500

    Create test-progress.R (#83)

---
+++ tests/testthat/test-progress.R
@@ -0,0 +1,15 @@
+test_that("progress_bar()", {
+  con <- file()
+  pb <- progress_bar(con = con)
+  for (i in 1:10 / 10) {
+    pb$set(i)
+  }
+  pb$set(i)
+  expect_true(length(readLines(con, warn = FALSE)) > 10)
+})
+
+test_that("progress_bar() errors", {
+  expect_error(progress_bar(char = character()))
+  expect_error(progress_bar(char = 1:2))
+  expect_error(progress_bar(char = NA))
+})
