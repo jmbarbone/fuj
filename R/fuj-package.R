9e0abbffb994bda1e67aba353a8f725191afdd5c
Jordan Mark Barbone <jmbarbone@gmail.com>
2026-01-19 23:59:11 -0500

    adds option to turn off lists (#91)

--- R/fuj-package.R
+++ R/fuj-package.R
@@ -27,3 +27,39 @@ op.fuj <- list(
   # set options
   options(op.fuj[!names(op.fuj) %in% names(options())]) # nocov
 }
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+#' `{fuj}` options
+#'
+#' Options uses for `{fuj}` functions.
+#'
+#' @keywords internal
+#' @examples
+#' names(op.fuj)
+#' op.fuj
+#' @export
+# nolint next: object_name_linter.
+op.fuj <- list(
+  fuj.list.active = TRUE,
+  fuj.vap.progress = FALSE,
+  fuj.vap.indexed_errors = FALSE,
+  fuj.verbose = NULL,
+  fuj.verbose.fill = FALSE,
+  fuj.verbose.label = "verbose: "
+)
+
+.onLoad <- function(libname, pkgname) {
+  # set options
+  options(op.fuj[!names(op.fuj) %in% names(options())]) # nocov
+  # try(globalCallingHandlers(`fuj:verbose_message` = fuj_verbose_handler))
+
+  if (isFALSE(getOption("fuj.list.active", TRUE))) {
+    list0 <<- list
+    lst <<- list
+  }
+}
