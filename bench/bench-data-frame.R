240dba32b7cb4592c5ea25a6980ee4cd0a400be2
Jordan Mark Barbone <jmbarbone@gmail.com>
2026-01-24 21:39:32 -0500

    adds benchmarks (#91)

--- bench/bench-data-frame.R
+++ bench/bench-data-frame.R
@@ -1,38 +1,34 @@
 devtools::load_all(here::here())
-library(ggplot2)
 library(bench)
 
-print(mark(
+plot(print(mark(
   quick_df(),
   quick_df(list()),
   dataframe(), # same as quick_df(NULL)
 
   data.frame(),
   data.frame(NULL),
-  data.frame(list())
-))
+  data.frame(list()),
+  mutframe()
+)))
 
-autoplot(.Last.value)
-
-print(mark(
+plot(print(mark(
   quick_df(list(a = integer(), b = character())),
   dataframe(a = integer(), b = character(), ),
-  data.frame(a = integer(), b = character())
-))
-
-autoplot(.Last.value)
+  data.frame(a = integer(), b = character()),
+  mutframe(a = integer(), b = character())
+)))
 
 local({
   c <- b <- a <- seq_len(100)
-  print(mark(
+  plot(print(mark(
     quick_df(list(a = a, b = b, c = c)),
     dataframe(a = a, b = b, c = c, ),
     data.frame(a = a, b = b, c = c),
     data.frame(a, b, c),
-  ))
+    mutframe(a = a, b = b, c = c)
+  )))
 })
-
-autoplot(.Last.value)
 
 local({
   library(plyr, include.only = "quickdf")
@@ -48,4 +44,11 @@ local({
     mutframe = mutframe(a = 1:n, b = 1:n, c = 1:n, x = sl),
     times = 1000
   )
+  plot(print(mark(
+    quick_df(list(a = 1:n, b = 1:n, c = 1:n, x = sl)),
+    quickdf(list(a = 1:n, b = 1:n, c = 1:n, x = sl)),
+    data.frame(a = 1:n, b = 1:n, c = 1:n, x = sl),
+    dataframe(a = 1:n, b = 1:n, c = 1:n, x = sl),
+    mutframe(a = 1:n, b = 1:n, c = 1:n, x = sl)
+  )))
 })
