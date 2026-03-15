f320fb628c92d81cb4aa2ef21e17a841a37ab89a
Jordan Mark Barbone <jmbarbone@gmail.com>
2026-03-15 13:23:14 -0400

    adds not() and negate() (#84)

---
+++ R/negate.R
@@ -0,0 +1,38 @@
+#' Negations
+#'
+#' Negate an outcome or function
+#'
+#' @description [fuj::not()] is an alias for [base::!].  [fuj::negate()] is
+#' a variation of [base::Negate()] with an extra steps to preserve the original
+#' function and maintain the function formals.
+#'
+#' @examples
+#' identical(not(TRUE), FALSE)
+#'
+#' different <- negate(identical)
+#' different(formals(identical), formals(identical))
+#'
+#' @name negate
+NULL
+
+#' @rdname negate
+#' @export
+#' @param x An object
+#' @returns [fuj::not()] See [base::!]
+not <- base::`!`
+
+#' @rdname negate
+#' @export
+#' @param fun A function
+#' @returns [fuj::negate()] A `function`, which is the negation of `fun`
+negate <- function(fun) {
+  forms <- formals(match.fun(fun))
+  fun <- substitute(fun)
+  neg <- function() {}
+  formals(neg) <- forms
+  body(neg) <- substitute(
+    not(do.call(..fun.., ..params..)),
+    list(..fun.. = fun, ..params.. = lapply(names(forms), str2lang))
+  )
+  neg
+}
