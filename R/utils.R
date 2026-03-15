05f4fd18023d193c6be4dc63ca5ae30c0697041e
Jordan Mark Barbone <jmbarbone@gmail.com>
2026-03-15 17:02:55 -0400

    update sutils (#87)

--- R/utils.R
+++ R/utils.R
@@ -1,15 +1,40 @@
-with_options <- function(op, code) {
-  op <- do.call(options, as.list(op))
-  on.exit(options(op))
-  force(code)
+#' Utilities
+#'
+#' Additional utilities
+#'
+#' @param x A vector of values
+#' @param pair A named list of values
+#' @param expr Expression to evaluate\
+#'
+#' @name fuj_utils
+#' @keywords internal
+#' @noRd
+NULL
+
+#' @rdname fuj_utils
+#' @noRd
+remove_na <- function(x) {
+  x[!is.na(x)]
 }
 
+#' @rdname fuj_utils
+#' @noRd
+with_options <- function(pair, expr) {
+  op <- do.call(options, as.list(pair))
+  on.exit(options(op))
+  force(expr)
+}
+
+#' @rdname fuj_utils
+#' @noRd
 pairlist_to_string <- function(pair) {
   nms <- names(pair)
   vals <- as.character(pair)
   paste(nms, "=", vals, collapse = ", ")
 }
 
+#' @rdname fuj_utils
+#' @noRd
 integerish <- function(x) {
   is.integer(x) ||
     (is.numeric(x) &&
