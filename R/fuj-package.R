07c59cf8b3bcb2a7b931fa98f2c3a85446dc3ddf
Jordan Mark Barbone <jmbarbone@gmail.com>
2026-03-19 00:11:13 -0400

    checks operating systems once on load (#99)

--- R/fuj-package.R
+++ R/fuj-package.R
@@ -24,13 +24,19 @@ op.fuj <- list(
 )
 
 .onLoad <- function(libname, pkgname) {
-  # set options
-  options(op.fuj[!names(op.fuj) %in% names(options())]) # nocov
-
   # nocov start
+  options(op.fuj[!names(op.fuj) %in% names(options())])
+
   if (isFALSE(getOption("fuj.list.active", TRUE))) {
     list0 <<- base::list
     lst <<- base::list
   }
+
+  switch(
+    Sys.info()[["sysname"]],
+    Windows = is_windows_ <<- TRUE,
+    Darwin = is_macos_ <<- TRUE,
+    Linux = is_linux_ <<- TRUE
+  )
   # nocov end
 }
